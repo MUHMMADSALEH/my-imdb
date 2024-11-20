884e9f1af6bca76ae560e91a217cff1c8dbf819d
MUHMMADSALEH <khanmohdsaleh2002@gmail.com>
2024-11-20 21:53:15 +0530

    feat:fetch data from tmdb and populated it usin results and card Component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
 import Providers from './Providers'
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <Providers>
 
         <Header/>
+        <Navbar/>
           {children}
         </Providers>
        </body>
